8b97bbd2541555d59c32782cff599971ef115a75
Ryoocht <ryo1.t.zubu@gmail.com>
2023-03-16 23:56:48 +1000

    added: Prisma connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './module/user/user.module';
+import { PrismaModule } from './module/prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, UserModule],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
