fba96876d0438f2a57b22fdfa7595a54c43e583a
Ryoocht <ryo1.t.zubu@gmail.com>
2023-03-21 00:10:52 +1000

    updated: dateUtil function error message

--- src/util/date-util/date-util.ts
+++ src/util/date-util/date-util.ts
@@ -1,10 +1,19 @@
+import { BadRequestException } from '@nestjs/common';
+
 export default class DateUtil {
   static transformInputDate(date: string) {
-    const regexp = new RegExp('([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))')
-    const isValidateDate = regexp.test(date)
-    if (!isValidateDate) return false
-    const transformDate = new Date(date)
+    const regexp = new RegExp(
+      '([12]d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]d|3[01]))',
+    );
+    const isValidateDate = regexp.test(date);
 
-    return transformDate
+    if (!isValidateDate)
+      throw new BadRequestException(
+        'Invalid date format. Input Date must be in YYYY-MM-DD.',
+      );
+
+    const transformDate = new Date(date);
+
+    return transformDate;
   }
 }
